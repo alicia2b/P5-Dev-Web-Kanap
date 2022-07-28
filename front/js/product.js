20d01b74cc816b4b50265cc44f25d3e7f444fa81
graziani <alicia.graziani.ag@gmail.com>
2022-07-28 17:06:58 +0200

    localsotrage quantite

--- front/js/product.js
+++ front/js/product.js
@@ -1,99 +1,62 @@
-function getOneProduct() {
-  const recherche= window.location.search;
-  const urlParams= new URLSearchParams(recherche);
-  const product_ID= urlParams.get('id');
+const id = new URLSearchParams(window.location.search).get('id');
 
-fetch("http://localhost:3000/api/products/"+product_ID)
+
+const colorsElement = document.querySelector('#colors');
+const addToCartButton = document.querySelector('#addToCart');
+const quantityElement = document.querySelector('#quantity');
+
+ 
+fetch("http://localhost:3000/api/products/"+id)
 .then(response => response.json())
 .then(product=>{
   document.querySelector('#title').innerHTML=product.name;
   document.querySelector('#price').innerHTML=product.price;
   document.querySelector('#description').innerHTML=product.description;
   document.querySelector('.item__img').innerHTML=`<img src="${product.imageUrl}" alt="${product.altTxt}">` 
+  
+for (let color of product.colors) { 
+colorsElement.insertAdjacentHTML('beforeend',`<option value="${color}">${color}</option>`);
+}
+})
 
- 
-})}
-
-
-
-
-
-
-
-
-
-
-/*function getOneProduct(){
-    const recherche = window.location.search;
-    const urlParams = new URLSearchParams(recherche);
-    const product_ID = urlParams.get('id');
-    
-    
-fetch("http://localhost:3000/api/products/"+product_ID)
-.then(response => response.json())
-.then(product =>{
-   document.querySelector('#title').innerHTML=product.name;
-   document.querySelector('#price').innerHTML=product.price;
-   document.querySelector('#description').innerHTML=product.description;
-   document.querySelector('.item__img').innerHTML=
-   `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
-    let optionTpl= `<option value="vert">vert</option>`;
-    console.log(product)
-    for (let i=0; i<product.colors.length;i++){
-        let optionColor=document.createElement('option');
-        optionColor.value=product.colors[i];
-        optionColor.innerHTML=product.colors[i];
-        document.querySelector('#colors').appendChild(optionColor)
+addToCartButton.addEventListener('click',e=>{
+const quantity= +quantityElement.value;
+const color = colorsElement.value;
+if (quantity<=0 || quantity>100|| color==''){
+  alert('veuillez selectionner une quantité et une couleur');
+}
+  else {
+    let cart = localStorage.getItem('cart');
+    if(cart==null){
+      let cartItems=[];
+      cartItems.push({
+        id:id,
+        color:color,
+        quantity:quantity
+      });
+      localStorage.setItem('cart',JSON.stringify(cartItems));
     }
-});
-}
-
-/*evenement quand la page est chargée*/
-window.onload= function(){
-    document.getElementById('addToCart').onclick=addToCart;
-    getOneProduct();
-}
-
-function addToCart(){
-    const recherche = window.location.search;
-    const urlParams = new URLSearchParams(recherche);
-    const product_ID = urlParams.get('id');
-    let color=  document.getElementById('colors').value;
-    let quantity= document.getElementById('quantity').value;
-
-    if(color==""){
-        alert('Veuillez choisir votre couleur');
-        return;
+    else {
+      let cartItems = JSON.parse(cart);
+      let productIndex= cartItems.findIndex(product=>product.id==id && product.color==color);
+      if (productIndex<0)
+      {
+        cartItems.push({
+          id:id,
+          color:color,  
+          quantity:quantity,
+        });
+      }
+        else{
+          cartItems[productIndex].quantity+= quantity;
+        }
+        localStorage.setItem('cart',JSON.stringify(cartItems));
+        alert('produit ajouté avec succès');
+      }
     }
-    if(quantity==0){
-        alert('Veuillez choisir une quantité');
-        return;
-    }
-    
-    addProduct(product_ID,quantity,color);
 }
+);
 
-function groupBy(product_ID) {
-    return getCart().reduce((groups, item) => {
-      let final = (groups[item.product_ID] || []),
-        sameIdAndColorIndex = final.map(elem => {
-          if (elem.product_ID == item.product_ID &&
-            elem.color == item.color)
-            return elem.product_ID;
-        }).indexOf(item.product_ID);
-      if (sameIdAndColorIndex == -1)
-        final.push(item);
-      else 
-        final[sameIdAndColorIndex].quantity += item.quantity;
-      groups[item.product_ID] = final;
-      return groups;
-    }, {});
-}
-
-
-//le paramètre "product_ID" de la fonction n'est plus utilisé, tu peux le supprimer à moins que si tu veux que la même fonction gère le regroupement par couleur par exemple...
-let groupedById = groupBy();
-console.log("original Cart :", getCart(), ", grouped By ID Cart :", groupedById);
 
 
 
--- front/js/storage.js
+++ front/js/storage.js
@@ -16,20 +16,3 @@ let cart=getCart();
 cart.push(line);
 setCart(cart);
 }
-function groupBy(product_ID) {
-    return getCart().reduce((groups, item) => {
-      let final = (groups[item.product_ID] || []),
-        sameIdAndColorIndex = final.map(elem => {
-          if (elem.product_ID == item.product_ID &&
-            elem.color == item.color)
-            return elem.product_ID;
-        }).indexOf(item.product_ID);
-      if (sameIdAndColorIndex == -1)
-        final.push(item);
-      else 
-        final[sameIdAndColorIndex].quantity += item.quantity;
-      groups[item.product_ID] = final;
-      return groups;
-    }, {});
-}
-  
